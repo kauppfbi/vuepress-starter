d1ce4e3a44c66d469e438b1bae46c71f813150ca
Fabian K <fabian.kaupp@mail.de>
2019-08-11 18:57:37 +0200

    add navbar and sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,16 @@
 module.exports = {
   base: '/vuepress-starter/',
   title: 'VuePress in Github Pages',
-  description:
-    'Static website built with VuePress and deployed on Github Pages',
+  description: 'Static website built with VuePress and deployed on Github Pages',
   dest: './docs',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Repo', link: 'https://github.com/kauppfbi/vuepress-starter' },
+    ],
+    sidebar: {
+      '/guide/': ['', 'prettier', 'renovate', 'circleci'],
+    },
+  },
 };
